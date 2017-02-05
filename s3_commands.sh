25f082215ffc5b381fda827ec450ebfe1ffb340a
Krishna Thapa <thapakrish@gmail.com>
2017-02-05 18:12:59 +0000

    script to submit spark job

--- s3_commands.sh
+++ s3_commands.sh
@@ -6,3 +6,6 @@ aws s3 rm --recursive s3://kt-wiki/dest-dir
 
 # Sync dirs from local to s3
 aws s3 sync source s3://kt-wiki/dest
+
+# list file objects
+aws s3api list-objects --bucket kt-wiki --query Contents[].[Key,Size]>> s3fsize.txt
